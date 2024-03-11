a3ff93c505d93b00b57d565934f913c22e0e784e
Doug Forbes <douglaskforbes@gmail.com>
2024-03-11 20:00:46 +0000

    added Poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +22,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${poppins.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
--- src/components/TopBannerVideo/TopBannerVideo.tsx
+++ src/components/TopBannerVideo/TopBannerVideo.tsx
@@ -6,7 +6,7 @@ const TopBannerVideo = () => {
       <div className="relative">
         <VideoPlayer src="/video/atedo_banner.mp4" />
         <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center pt-[15%]">
-          <h1 className="text-white text-6xl lg:text-9xl md:text-8xl">ATEDO.</h1>
+          <h1 className="tracking-wider text-white text-6xl lg:text-9xl md:text-8xl">ATEDO.</h1>
         </div>
       </div>
     </div>
