b499c48394d7e2b54e6055a95bf4b5ebb20c70c5
carey-andrew <carey_andrew@hotmail.com>
2024-03-21 12:57:00 +0000

    styling save

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,31 +2,54 @@
 // a section with icon links to instagram and tiktok and an email icon
 // include a quick link back to top of page called home, a link to the projects section, where the videos are, a gallery link and an about section
 
-import React from 'react';
-import ScrollIntoView from '../scrollIntoView/ScrollIntoView';
+import React from "react";
+import ScrollIntoView from "../scrollIntoView/ScrollIntoView";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer mx-auto px-4">
       <ScrollIntoView>
-        <h2 className="footer__heading">ATEDO.</h2>
+        <h2 className="flex align-left text-3xl font-bold text-black mt-8 mb-4" 
+        >READY TO GET IN TOUCH?</h2>
+        <section className="links border-x-indigo-500">
+          <a href="mailto: />" >Contact Us</a>
+        </section>
+        <section>
+          <a href="https://www.instagram.com/">
+            <Image
+              src="/icons/instagram-light.png"
+              alt="Instagram"
+              width={30}
+              height={30}
+            />
+          </a>
+          <a href="https://www.tiktok.com/">
+            <Image
+              src="/icons/tiktok-light.png"
+              alt="TikTok"
+              width={30}
+              height={30}
+            />
+          </a>
+          <a href="mailto: />">
+            <Image
+              src="/icons/email-light.png"
+              alt="Email"
+              width={30}
+              height={30}
+            />
+          </a>
+        </section>
+        <section className="flex space-x-2 justify-center">
+          <a href="#home">Home</a>
+          <a href="#projects">Projects</a>
+          <a href="#gallery">Gallery</a>
+          <a href="#about">About</a>
+        </section>
       </ScrollIntoView>
-      <section className="footer__section">
-        <a href="mailto: />" className="footer__link">Contact Us</a>
-        </section>
-        <section className="footer__section">
-        <a href="https://www.instagram.com/" className="footer__link">Instagram</a>
-        <a href="https://www.tiktok.com/" className="footer__link">TikTok</a>
-        <a href="mailto: />" className="footer__link">Email</a>
-        </section>
-        <section className="footer__section">
-        <a href="#home" className="footer__link">Home</a>
-        <a href="#projects" className="footer__link">Projects</a>
-        <a href="#gallery" className="footer__link">Gallery</a>
-        <a href="#about" className="footer__link">About</a>
-        </section>
     </footer>
-    );
-}
+  );
+};
 
 export default Footer;
