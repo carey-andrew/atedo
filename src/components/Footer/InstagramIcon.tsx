db29e82493c78d70cf0ee027a3f808e1092f0784
Doug Forbes <douglaskforbes@gmail.com>
2024-03-28 09:33:41 +0000

    Updated social links

--- src/components/Footer/InstagramIcon.tsx
+++ src/components/Footer/InstagramIcon.tsx
@@ -7,7 +7,7 @@ const InstagramIcon = () => {
   const { theme } = useTheme();
 
   return (
-    <a href="https://www.instagram.com/" className="p-2">
+    <a href="https://www.instagram.com/p/CCvyMbCJdlT/" target="_blank" className="p-2">
       <Image src={theme == "light" ? "/icons/instagram-dark-df.png" : "/icons/instagram-light-df.png"} alt="Instagram" width={50} height={50} />
     </a>
   );
