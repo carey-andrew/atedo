010b19825c5f5471fb4b3cca62f80a9714d82793
Doug Forbes <douglaskforbes@gmail.com>
2024-04-05 12:36:37 +0100

    got first image from Instagram

--- src/components/InstagramGallery/InstagramGallery.tsx
+++ src/components/InstagramGallery/InstagramGallery.tsx
@@ -1,30 +1,32 @@
-"use client";
-
+import Image from "next/image";
 import { useEffect } from "react";
 
-const InstagramGallery = () => {
-  useEffect(() => {
-    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_TOKEN}`;
-    console.log("url: ", url);
-    async function fetchData() {
-      try {
-        const response = await fetch("url");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        // setData(fetchedData);
-      } catch (error) {
-        // setError(error);
-      } finally {
-        // setLoading(false);
-      }
+const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_TOKEN}`;
+
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-
-    fetchData();
-  }, []);
-
-  return <div>InstagramGallery</div>;
+    return await response.json();
+  } catch (error) {
+    // setError(error);
+    console.log("in catch");
+  }
+  //   } finally {
+  //     console.log("In finally");
+  //     // setLoading(false);
+  //   }
+}
+const InstagramGallery = async () => {
+  const instagramData = await fetchData();
+  console.log("instagramData: ", instagramData.data[0].media_url);
+  return (
+    <>
+      <img src={instagramData.data[0].media_url} className="w-[200px]"></img>
+    </>
+  );
 };
 
 export default InstagramGallery;
